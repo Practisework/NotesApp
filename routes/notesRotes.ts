52b9ab43e7504f532df6a9285ba7d70076719a17
Practisework <Acplwork77@gmail.com>
2024-06-07 11:57:34 +0530

    modified:   routes/notesRotes.ts 	modified:   views/createNote.ejs 	new file:   views/editNotes.ejs 	modified:   views/indeNote.ejs 	modified:   views/notes.ejs

--- routes/notesRotes.ts
+++ routes/notesRotes.ts
@@ -23,13 +23,44 @@ router.post("/createNote", (req: any, res: any) => {
   newNote
     .save()
     .then(() => {
-      res.redirect("/");
+      res.redirect("/notes");
     })
     .catch((err: any) => {
       res.send(err);
     });
 });
 
+router.post("/delete/:id", (req: any, res: any) => {
+  Note.Note.findById(req.params.id)
+    .then((note: any) => {
+      Note.Note.deleteOne(note).then(() => {
+        res.redirect("/notes");
+      });
+    })
+    .catch((err: any) => {
+      res.send(err);
+    });
+});
+router.get("/edit/:id", (req: any, res: any) => {
+  Note.Note.findById(req.params.id)
+    .then((note: any) => {
+      res.render("editNotes", { NoteID: note._id, data: note.body });
+    })
+    .catch((err: any) => {
+      res.send(err);
+    });
+});
+router.post("/edit/:id", (req: any, res: any) => {
+  Note.Note.findById(req.params.id)
+    .then((note: any) => {
+      Note.Note.updateOne(note, { body: req.body.noteBody }).then(() => {
+        res.redirect("/notes");
+      });
+    })
+    .catch((err: any) => {
+      res.send(err);
+    });
+});
 router.get("/:id", (req: any, res: any) => {
   Note.Note.findById(req.params.id)
     .then((note: any) => {
